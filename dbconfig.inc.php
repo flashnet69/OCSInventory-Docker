efaa60eafa050d6f2331724c16a500effc423ed5
paullepetit <plepettit@gmail.com>
2017-02-09 16:05:50 +0100

    Update container's name

--- dbconfig.inc.php
+++ dbconfig.inc.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_NAME", "ocsweb");
-define("SERVER_READ","ocsinventorydocker_db_1");
-define("SERVER_WRITE","ocsinventorydocker_db_1");
+define("SERVER_READ","ocsinventory-db");
+define("SERVER_WRITE","ocsinventory-db");
 define("COMPTE_BASE","ocs");
 define("PSWD_BASE","ocs");
 $_SESSION["PSWD_BASE"]="ocs";
